64e22eafdecd1c245826e2aa314af08422a868ad
David <davidschlachter96@gmail.com>
2021-04-24 01:01:45 -0700

    Initial commit. Old School Revision 195, decompiled cs2 in kotlin-a-like syntax.

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "com.javatar.cs2"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "kotlin-cs2-scripts"
+
